664a05ffbc5936b1b4dcd09b78863fffe89e8c3a
Grecagni <151087457+Grecagni@users.noreply.github.com>
2024-08-24 14:05:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js')
+            navigator.serviceWorker.register('/gestione-spese-2/service-worker.js')
                 .then(registration => {
                     console.log('Service Worker registrato con successo:', registration);
                 })

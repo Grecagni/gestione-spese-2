0445d05bbd1dd65fdae910918ce65a99dee4ac77
Grecagni <151087457+Grecagni@users.noreply.github.com>
2024-08-24 16:51:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,23 @@
-const CACHE_NAME = "spese-cache-v1";
-const urlsToCache = [
-    "/gestione-spese-2/",
-    "/gestione-spese-2/index.html",
-    "/gestione-spese-2/add-expense.html",
-    "/gestione-spese-2/expenses.html",
-    "/gestione-spese-2/style.css",
-    "/gestione-spese-2/app.js",
-    "/gestione-spese-2/manifest.json",
-    "/gestione-spese-2/icon-192x192.png",
-    "/gestione-spese-2/icon-512x512.png"
-];
-
-self.addEventListener("install", event => {
+self.addEventListener('install', (event) => {
+    console.log('Service worker installato');
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('gestione-spese-cache').then((cache) => {
+            return cache.addAll([
+                '/gestione-spese-2/',
+                '/gestione-spese-2/index.html',
+                '/gestione-spese-2/add-expense.html',
+                '/gestione-spese-2/expenses.html',
+                '/gestione-spese-2/style.css',
+                '/gestione-spese-2/app.js'
+            ]);
+        })
     );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-self.addEventListener("activate", event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
